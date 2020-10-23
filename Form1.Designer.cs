5baa30ffb9c0ea09d1da647e394753b691f9b498
Mathew Boisvert <msbpro@gmail.com>
2020-10-22 21:07:57 -0400

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WakeyWakeyUI
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.tmrCheckStatus = new System.Windows.Forms.Timer(this.components);
+            this.tmrUpdateAway = new System.Windows.Forms.Timer(this.components);
+            this.lblTimer2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimer.Location = new System.Drawing.Point(12, 9);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(64, 17);
+            this.lblTimer.TabIndex = 0;
+            this.lblTimer.Text = "00:00:00";
+            // 
+            // tmrCheckStatus
+            // 
+            this.tmrCheckStatus.Enabled = true;
+            this.tmrCheckStatus.Interval = 1000;
+            this.tmrCheckStatus.Tick += new System.EventHandler(this.tmrCheckStatus_Tick);
+            // 
+            // tmrUpdateAway
+            // 
+            this.tmrUpdateAway.Enabled = true;
+            this.tmrUpdateAway.Interval = 800;
+            this.tmrUpdateAway.Tick += new System.EventHandler(this.tmrUpdateAway_Tick);
+            // 
+            // lblTimer2
+            // 
+            this.lblTimer2.AutoSize = true;
+            this.lblTimer2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimer2.Location = new System.Drawing.Point(82, 9);
+            this.lblTimer2.Name = "lblTimer2";
+            this.lblTimer2.Size = new System.Drawing.Size(64, 17);
+            this.lblTimer2.TabIndex = 1;
+            this.lblTimer2.Text = "00:00:00";
+            this.lblTimer2.Click += new System.EventHandler(this.lblTimer2_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(158, 30);
+            this.Controls.Add(this.lblTimer2);
+            this.Controls.Add(this.lblTimer);
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.ShowIcon = false;
+            this.Text = "WW";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Timer tmrCheckStatus;
+        private System.Windows.Forms.Timer tmrUpdateAway;
+        private System.Windows.Forms.Label lblTimer2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace WakeyWakeyUI
+{
+    public partial class frmMain : Form
+    {
+        [FlagsAttribute]
+        public enum EXECUTION_STATE : uint
+        {
+            ES_AWAYMODE_REQUIRED = 0x00000040,
+            ES_CONTINUOUS = 0x80000000,
+            ES_DISPLAY_REQUIRED = 0x00000002,
+            ES_SYSTEM_REQUIRED = 0x00000001
+        }
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct LASTINPUTINFO
+        {
+            public static readonly int SizeOf = Marshal.SizeOf(typeof(LASTINPUTINFO));
+
+            [MarshalAs(UnmanagedType.U4)]
+            public int cbSize;
+            [MarshalAs(UnmanagedType.U4)]
+            //public UInt32 dwTime;
+            public int dwTime;
+        }
+
+        [DllImport("user32.dll")]
+        static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+        [DllImport("user32.dll")]
+        public static extern void keybd_event(
+        byte bVk,
+        byte bScan,
+        uint dwFlags,
+        uint dwExtraInfo
+        );
+
+        const int VK_CONTROL = 0x11;
+        const uint KEYEVENTF_KEYUP = 0x2;
+
+        private int _lastTime;
+        private DateTime _dtCalc;
+        private DateTime _dtCalc2;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            int tmrTime = int.Parse(ConfigurationManager.AppSettings["TimerTime"]);
+            tmrUpdateAway.Interval = tmrTime;
+            _dtCalc = DateTime.Now;
+            _dtCalc2 = DateTime.Now;
+            KeepAlive();
+        }
+
+        private void KeepAlive()
+        {
+            SetThreadExecutionState(EXECUTION_STATE.ES_DISPLAY_REQUIRED | EXECUTION_STATE.ES_SYSTEM_REQUIRED | EXECUTION_STATE.ES_AWAYMODE_REQUIRED | EXECUTION_STATE.ES_CONTINUOUS);
+            keybd_event((byte)VK_CONTROL, 0, KEYEVENTF_KEYUP, 0);
+            int lastInputTime = GetLastInputTime();
+            Debug.WriteLine(String.Format("Keep Alive Triggered, lastInputTime {0} / _lastTime {1}", lastInputTime, _lastTime));
+            if (lastInputTime < _lastTime)
+            {
+                _dtCalc = DateTime.Now;
+            }
+            _lastTime = lastInputTime;
+        }
+
+        private int GetLastInputTime()
+        {
+            int idleTime = 0;
+            LASTINPUTINFO lastInputInfo = new LASTINPUTINFO();
+            lastInputInfo.cbSize = Marshal.SizeOf(lastInputInfo);
+            lastInputInfo.dwTime = 0;
+
+            int envTicks = Environment.TickCount;
+
+            if (GetLastInputInfo(ref lastInputInfo))
+            {
+                int lastInputTick = lastInputInfo.dwTime;
+                idleTime = envTicks - lastInputTick;
+            }
+            return ((idleTime > 0) ? (idleTime / 1000) : 0);
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS);
+        }
+
+        private void tmrCheckStatus_Tick(object sender, EventArgs e)
+        {
+            lblTimer.Text = (DateTime.Now - _dtCalc).ToString(@"hh\:mm\:ss");
+            lblTimer2.Text = (DateTime.Now - _dtCalc2).ToString(@"hh\:mm\:ss");
+
+            int lastInputTime = GetLastInputTime();
+            if (lastInputTime < _lastTime)
+            {
+                _dtCalc = DateTime.Now;
+            }
+            _lastTime = lastInputTime;
+        }
+
+        private void tmrUpdateAway_Tick(object sender, EventArgs e)
+        {
+            KeepAlive();
+        }
+
+        private void lblTimer2_Click(object sender, EventArgs e)
+        {
+            _dtCalc2 = DateTime.Now;
+        }
+
+        
+    }
+}
